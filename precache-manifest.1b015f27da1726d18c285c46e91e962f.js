6a58b57c908d78e4076505a3c5440b84433700cf
Vitaliy Derda <derdavitaliy@gmail.com>
2020-07-31 23:50:11 +0300

    Updates

---
+++ precache-manifest.1b015f27da1726d18c285c46e91e962f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "241cbb718d95e68dad599ec839a5a22d",
+    "url": "/my-cv/index.html"
+  },
+  {
+    "revision": "45979051c90f83ef837b",
+    "url": "/my-cv/static/css/main.db78101b.chunk.css"
+  },
+  {
+    "revision": "0c863918724e08071dd5",
+    "url": "/my-cv/static/js/2.00a04cce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-cv/static/js/2.00a04cce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45979051c90f83ef837b",
+    "url": "/my-cv/static/js/main.d44030f1.chunk.js"
+  },
+  {
+    "revision": "59b714c33bdd7cb64d9c",
+    "url": "/my-cv/static/js/runtime-main.c75d1202.js"
+  },
+  {
+    "revision": "702052cb935f2f58d3584d5ba43b33e9",
+    "url": "/my-cv/static/media/photo.702052cb.png"
+  }
+]);
